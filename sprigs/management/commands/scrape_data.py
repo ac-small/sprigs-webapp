4c91e5a6f324a28c4811ccf34a3d5a0948682144
Alexis S <alexis.small@canada.ca>
2020-03-30 15:05:13 -0400

    included product description to help sort in category breakouts

--- sprigs/management/commands/scrape_data.py
+++ sprigs/management/commands/scrape_data.py
@@ -12,10 +12,18 @@ product_list = []
 classification_list = []
 global data
 
-def flipp_request(location, merchant):
+def metadata_request(location, merchant):
     req = requests.get('https://backflipp.wishabi.com/flipp/items/search?locale=en-ca&postal_code='+ location + '&radius=50km' + '&q=' + merchant)
     res = req.json()['items']
     return res
+
+def description_request(flyer_item_id):
+    req = requests.get('https://backflipp.wishabi.com/flipp/items/'+ str(flyer_item_id))
+    res = req.json()['item']['description']
+    if res is not None:
+        return res.upper()
+    else:
+        return ""
 
 class Command(BaseCommand):
     help = 'Populates DB with product data'
@@ -24,14 +32,23 @@ class Command(BaseCommand):
         # retrieve flyer data
         for loc in LOCATION:
             for mer in MERCHANTS:
-                data = flipp_request(loc,mer)
+                data = metadata_request(loc,mer)
 
                 # loop over data, format, and extract values of interest
                 for i in data:
+                    # if item falls into a poultry category, run additional request to
+                    # retrieve description information (specifics on product ie. boneless, skinless etc).
+                    pname = i['name'].upper()
+                    if 'CHICKEN' in pname or 'TURKEY' in pname or 'DUCK' in pname:
+                        full_name = pname + " " + description_request(i['flyer_item_id'])
+                    else:
+                        full_name = pname
+                    
+                    # Create product objects, and persist to database
                     Product.objects.bulk_create([
                         Product(
-                        flyer_id=i['flyer_item_id'],
-                        product_name=i['name'].upper(),
+                        flyer_id = i['flyer_item_id'],
+                        product_name = full_name,
                         merchant = i['merchant_name'].upper(),
                         sale_price = i['current_price'],
                         price_units = i['post_price_text'],

58e60893ded1fac201fbb2db9ea7ac77b53b53d9
Alexis S <alexis.small@canada.ca>
2020-04-03 14:15:24 -0400

    enhanced categories and save classification results to db

--- sprigs/management/commands/classify.py
+++ sprigs/management/commands/classify.py
@@ -13,15 +13,29 @@ import seaborn as sns
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Q
 from sprigs.models import Product
+from sprigs.models import Category
+from sprigs.models import Product_Classification
 
+# initialize prediction list
+classified_datalist = []
 
 def get_dataframe():
+    """
+    Read training dataset and save contents into a dataframe
+    """
     df = pd.read_csv("./sprigs/management/commands/training_dataset.csv", encoding = "ISO-8859-1")
     df.head()
     return df
 
-def train_and_classify(df, poultry_dataframe):
-    categories = ['Chicken Breast Boneless Skinless','Chicken Whole Birds (Fresh)','Chicken Whole Birds (Frozen)','Chicken Drumsticks','Chicken Leg Quarters','Chicken Leg','Chicken Thigh Boneless Skinless','Chicken Wings','Chicken Breast Bone-In Skin-On','Chicken Thigh Bone-In Skin-On']
+def retrieve_categories():
+    """
+    Query category table in database
+    Return a list of all categories
+    """
+    categories = list(Category.objects.values_list('category', flat=True))
+    return categories
+
+def train_and_classify(df, categories, poultry_dataframe):
     train, test = train_test_split(df, random_state=42, test_size=0.33, shuffle=True)
     X_train = train.TEXT
     X_test = test.TEXT
@@ -37,9 +51,8 @@ def train_and_classify(df, poultry_dataframe):
         # compute the testing accuracy
         prediction = training_model.predict(X_test)
         print('Test accuracy is {}'.format(accuracy_score(test[category], prediction)))
-        # predict external data
-        ext_preds = predict_data(training_model, category, poultry_dataframe)
-        print (ext_preds)
+        # run product data against trained model
+        classified_datalist.append(predict_data(training_model, category, poultry_dataframe))
 
 def predict_data(model, cat, poultry_dataframe):
     data_predictions = model.predict(poultry_dataframe.iloc[ : , 1 ])
@@ -49,20 +62,37 @@ def predict_data(model, cat, poultry_dataframe):
             predictions.append((poultry_dataframe.iloc[ idx , 0 ], cat))
     return predictions
 
+def save_results_to_db(data):
+    # Iterate over the array of tuples, create
+    # classification objects, and persist to database
+    for i in data:
+        for record in i:
+            print (record)
+            Product_Classification.objects.bulk_create([
+            Product_Classification(
+            product_id = record[0],
+            classification = record[1],
+        )])
+
+
 class Command(BaseCommand):
     help = 'Trains machine learning model, and predicts product classifications'
 
     def handle(self, *args, **options):
         """
+        Retrieve category and product values from database
         Filter products by those containing price units (lbs/kg), and contains keywords chicken, turkey, or duck
-        Returns: List of product objects (flyer_id, and product_name)
         """
+        categories = retrieve_categories()
         poultry_list = list(Product.objects.filter(price_units__isnull=False).filter(Q(product_name__icontains='CHICKEN')
             | Q(product_name__icontains='TURKEY')
             | Q(product_name__icontains='DUCK'))
             .values_list('flyer_id','product_name'))
 
         poultry_dataframe = pd.DataFrame(poultry_list)
-
+        """
+        Train model, classify results and save corresponding product classifications to database
+        """
         training_dataframe = get_dataframe()
-        train_and_classify(training_dataframe, poultry_dataframe)
+        train_and_classify(training_dataframe, categories, poultry_dataframe)
+        save_results_to_db(classified_datalist)

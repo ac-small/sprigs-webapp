be98952fbd77fffd91980648d30b9ba1f82b90a3
Alexis S <alexis.small@canada.ca>
2020-04-09 15:35:49 -0400

    debugging db string

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'azuresite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = { 'default': dj_database_url.config() }
 
 
 # Password validation

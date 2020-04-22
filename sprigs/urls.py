ed21227a32cb1b3c063c2811c92debccf45454b8
Alexis S <alexis.small@canada.ca>
2020-04-22 10:37:09 -0400

    replaced half functioning js datatables with basic html table

--- sprigs/urls.py
+++ sprigs/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'sprigs'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('category/', views.OrderListJson.as_view(), name='order_list_json'),
-    #path('category/result', views.ResultsView.as_view(), name='order_list_json'),
+    path('category/', views.ResultsView.as_view(), name='results'),
 
 ]
--- sprigs/views.py
+++ sprigs/views.py
@@ -1,11 +1,4 @@
-from django.shortcuts import get_object_or_404, render
-from django_datatables_view.base_datatable_view import BaseDatatableView
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 from django.views import generic
-from django.http import HttpResponse
-import numpy as np
-
 from .models import Product, Category, Product_Classification
 
 class IndexView(generic.ListView):
@@ -17,25 +10,15 @@ class IndexView(generic.ListView):
         """
         return Category.objects.exclude(category__isnull=True).exclude(category__exact='None').distinct('category')
 
-class OrderListJson(BaseDatatableView):
+class ResultsView(generic.ListView):
 
-    # The model we're going to show
-    model = Product
-    # define the columns that will be returned
-    columns = ['product_name', 'sale_price', 'start_date']
-    order_columns = ['product_name', 'sale_price', 'start_date']
-    max_display_length = 50
+    template_name = 'sprigs/results.html'
+    context_object_name = 'results'
 
-    def get_initial_queryset(self):
+    def get_queryset(self):
         selected_option = self.request.GET.get('category')
         classification_lookup = Product_Classification.objects.filter(classification__exact=selected_option).values("product_id")
-        return (Product.objects.filter(flyer_id__in=classification_lookup).values("product_name", "sale_price", "start_date").distinct())
-
-    def render_column(self, row, column):
-        return super(OrderListJson, self).render_column(row, column)
+        results = Product.objects.filter(flyer_id__in=classification_lookup).values("product_name", "sale_price", "start_date").distinct()
+        return results
 
         #TODO: generate visuals / graph. Use Matplotlib? Bokeh? Plotly?
-
-class ResultsView(generic.View):
-    def get(self, request):
-        return render(request, 'sprigs/results.html')

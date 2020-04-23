cc9d5e2f5e523305bfd9e72016db7db6a9083732
Alexis S <alexis.small@canada.ca>
2020-04-23 10:23:51 -0400

    heroku db settings

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -76,15 +77,7 @@ WSGI_APPLICATION = 'azuresite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sprigs',
-        'HOST': 'localhost',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres'
-        }
-}
+DATABASES = { 'default': dj_database_url.config() }
 
 
 # Password validation
